26c1afb9fa8fb905f7c95fd36ee5857f8725615f
daiyaebi <daiyaoil@icloud.com>
2024-12-08 15:22:38 +0900

    第７章まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,17 @@
 import { getNewsList } from "../_libs/microcms";
 import NewsList from "../_components/NewsList/index";
+import Pagination from "../_components/Pagination/index";
+import SearchField from "../_components/SearchField/index";
+import { NEWS_LIST_LIMIT } from "../_constants/index";
 export default async function Page(){
-    const {contents: news} = await getNewsList();
-    return <NewsList news={news}/>;
+    const {contents: news, totalCount} = await getNewsList({
+        limit: NEWS_LIST_LIMIT,
+    });
+    return (
+        <>
+            <SearchField />
+            <NewsList news={news}/>
+            <Pagination totalCount={totalCount} />
+        </>
+    );
 }
