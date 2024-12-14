88c5d61bd14636173984700c361aef603aa538a9
daiyaebi <daiyaoil@icloud.com>
2024-12-14 14:04:47 +0900

    9章完了

---
+++ app/contact/layout.tsx
@@ -0,0 +1,19 @@
+import Hero from "../_components/Hero/index";
+import Sheet from "../_components/Sheet/index";
+
+export const metadata = {
+  title: "コンタクト｜シンプルなコーポレートサイト",
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="Contact" sub="お問い合わせ" />
+      <Sheet>{children}</Sheet>
+    </>
+  );
+}
