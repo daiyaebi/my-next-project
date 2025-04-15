2404b62b40e1d5430a6cec8ef0ef0961e2b241ad
daiyaebi <daiyaoil@icloud.com>
2025-04-16 01:14:36 +0900

    20250415:微修正2
    20250415:微修正2

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -10,7 +10,7 @@ export const revalidate = 60;
 export default function NewsLayout({children}: Props){
     return (
         <>
-            <Hero title="News" sub="Knowledge" />
+            <Hero title="Knowledge" sub="Knowledge" />
             <Sheet>{children}</Sheet>
         </>
     );
