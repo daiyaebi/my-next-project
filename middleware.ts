a21687c6b17f2c2a0ba5f93c18a7eb66e2040e33
daiyaebi <daiyaoil@icloud.com>
2025-04-16 17:22:20 +0900

    Basic認証削除
    Basic認証削除

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { createNextAuthMiddleware } from "./node_modules/nextjs-basic-auth-middleware/dist/index";
-
-export const middleware = createNextAuthMiddleware();
-
-export const config = {
-    matcher: ["/(.*)"],
-};
