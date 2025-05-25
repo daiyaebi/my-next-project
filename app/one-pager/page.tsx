e706a7d178509766bbb37d50addfce54bb6df4d0
daiyaebi <daiyaoil@icloud.com>
2025-05-25 13:06:22 +0900

    修正3

--- app/one-pager/page.tsx
+++ app/one-pager/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
 import OnePagerClient from './OnePagerClient';
+import styles from './page.module.css';
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className={styles['buy-form']}>Loading...</div>}>
       <OnePagerClient />
     </Suspense>
   );
