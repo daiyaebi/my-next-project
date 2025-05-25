1dcdd9b471af10c9fe69d2ae9e2729f1b5a37b6b
daiyaebi <daiyaoil@icloud.com>
2025-05-25 13:39:42 +0900

    修正8

--- app/one-pager/page.tsx
+++ app/one-pager/page.tsx
@@ -4,6 +4,12 @@ import styles from './page.module.css';
 
 export default function Page() {
   return (
+  <>
+  <header className={styles['page-title-wrapper']}>
+     <h1 className={styles['page-title']}>
+       Shopify Storefront API × Next.js（ヘッドレスコマース）で「今すぐ購入」からチェックアウトにスムーズに遷移するミニアプリ
+     </h1>
+  </header>
 　<Suspense
   　fallback={
     　<div className={styles['loading-wrapper']}>
@@ -14,5 +20,6 @@ export default function Page() {
 　>
   　<OnePagerClient />
 　</Suspense>
+</>
   );
 }
