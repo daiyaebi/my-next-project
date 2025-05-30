5fc9e5f0239290f87324c1e9e232671b410c17a3
daiyaebi <daiyaoil@icloud.com>
2025-05-30 20:21:48 +0900

    APIのバージョンアップ

--- app/_libs/shopify.ts
+++ app/_libs/shopify.ts
@@ -2,7 +2,7 @@ export async function callShopifyCart(query: string, variables?: Record<string, 
     const shop = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ENDPOINT!;
     const token = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_TOKEN!;
   
-    const endpoint = `https://${shop}.myshopify.com/api/2024-04/graphql.json`;
+    const endpoint = `https://${shop}.myshopify.com/api/2025-01/graphql.json`;
   
     const res = await fetch(endpoint, {
       method: 'POST',
