3305b372b04fa00283443cadd3ecd4a6c002e80d
daiyaebi <daiyaoil@icloud.com>
2025-05-31 08:17:53 +0900

    コメントアウト解除

--- app/product-intro/productIntroClient.tsx
+++ app/product-intro/productIntroClient.tsx
@@ -204,7 +204,7 @@ export default function ProductIntroClient() {
 
       if (checkoutUrl) {
         console.log(checkoutUrl);
-        // window.location.href = checkoutUrl;
+        window.location.href = checkoutUrl;
       } else {
         throw new Error('チェックアウトURL取得失敗');
       }
