e807df90c727eb999c2d874599a9242e0678dcd8
daiyaebi <daiyaoil@icloud.com>
2025-12-07 14:32:14 +0900

    修正1

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,6 +3,8 @@ import NewsList from "../_components/NewsList/index";
 import Pagination from "../_components/Pagination/index";
 import SearchField from "../_components/SearchField/index";
 import { NEWS_LIST_LIMIT } from "../_constants/index";
+import styles from "./page.module.css";
+
 
 export default async function Page(){
     const {contents: news, totalCount} = await getNewsList({
@@ -11,6 +13,18 @@ export default async function Page(){
     return (
         <>
             <SearchField />
+            <a 
+              href="https://note.com/e_daiya" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <img 
+                src="/note_image.png" 
+                alt="note投稿記事" 
+                className={styles.noteImage}
+              />
+              <span>noteにも記事を投稿してます！</span>
+            </a>
             <NewsList news={news}/>
             <Pagination totalCount={totalCount} />
         </>
